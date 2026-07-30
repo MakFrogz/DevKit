65ffe9365bd3fed89851eeaab94c6b098c9097c9
Mark Khrizman <>
2026-07-30 21:13:20 +0300

    Singleton
    - Added plane c# singleton;
    - Added Mono and Persistent singletons for Unity components;

---
+++ Assets/Scripts/Singletons/MonoSingleton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Singletons
+{
+    public class MonoSingleton <T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+        private static readonly object _lock = new object();
+        
+        public static T Instance
+        {
+            get
+            {
+                if (_instance)
+                {
+                    return _instance;
+                }
+
+                lock (_lock)
+                {
+                    _instance = FindAnyObjectByType<T>();
+
+                    if (_instance)
+                    {
+                        return _instance;
+                    }
+                    
+                    var gameObject = new GameObject(typeof(T).Name + " - Singleton");
+                    _instance = gameObject.AddComponent<T>();
+                    return _instance;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Singletons/PersistentSingleton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Singletons
+{
+    public class PersistentSingleton<T> : MonoSingleton<T> where T : Component
+    {
+        private void Awake()
+        {
+            if (Application.isPlaying)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}
